06818c7c42002d693dc7e53b2ca9e988658c0da4
マツウィン（松文） <yanyingsong@outlook.com>
2021-10-15 14:31:20 +0800

    Update downgrade_test.go

--- downgrade_test.go
+++ downgrade_test.go
@@ -8,16 +8,24 @@ import (
 )
 
 func TestNew(t *testing.T) {
+
+	// 新建任务实例，期望一秒内完成，三次重试机会
 	work := New(time.Second, 3)
+
+	// 定义主要任务内容
 	work.Plan1 = func() error {
 		fmt.Println("run plan1")
 		return errors.New("plan1 error")
 	}
+
+	// 定义备选任务内容
 	work.Plan2 = func(err error) error {
 		fmt.Println(err)
 		fmt.Println("run plan2")
 		return nil
 	}
+
+	// 开始任务
 	if err := work.Do(); err != nil {
 		panic(err)
 	}
